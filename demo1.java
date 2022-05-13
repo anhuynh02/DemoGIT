528fcbe05bad4869b3f84da4aa56640fd98e9080
Huynh Tri Thong <anhuynh290102@gmail.com>
2022-05-13 22:55:06 +0700

    them ham main

--- demo1.java
+++ demo1.java
@@ -1,5 +1,6 @@
-
-public static double dev(double a, double b){
-	System.out.print(a/b)
-	return a/b
+public static void main(String args[]){
+	public static double dev(double a, double b){
+		System.out.print(a/b)
+		return a/b
+	}
 }
